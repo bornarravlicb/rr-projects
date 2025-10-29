4d8bc96331ecf95c1986890a1eadc88e0254b870
bornarravlicb <bornarravlicb@gmail.com>
2025-10-29 16:44:02 +0100

    Add interactivity to project cards on hover

---
+++ script.js
@@ -0,0 +1,6 @@
+// OPTIONAL JS FOR INTERACTIVITY
+document.querySelectorAll('.project-card').forEach(card => {
+  card.addEventListener('mouseenter', () => {
+    console.log('Hovered over:', card.querySelector('h3').innerText);
+  });
+});
